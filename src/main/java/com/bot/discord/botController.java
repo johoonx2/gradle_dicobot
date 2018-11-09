5827b1452225e69df6ee5f0e5e25bc0b89b37fca
hoonjo <hoonjo@linecorp.com>
2018-11-09 11:48:15 +0900

    [조훈] 테스트

--- src/main/java/com/bot/discord/botController.java
+++ src/main/java/com/bot/discord/botController.java
@@ -2,6 +2,9 @@ package com.bot.discord;
 
 public class botController {
     public static void main(String[] args){
-        System.out.println("HelloWorld");
+        if(args.length > 0)
+            System.out.println(args[0]);
+        else
+            System.out.println("HelloWorld");
     }
 }
